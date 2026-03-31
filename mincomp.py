c5ecef474b3a82c145b6e44c38e744dbd7a73d75
Sathwik-RS <rssathwik5@gmail.com>
2026-03-31 21:34:09 +0530

    new code

--- mincomp.py
+++ mincomp.py
@@ -3,10 +3,10 @@ num2=int(input("enter num2"))
 num3=int(input("enter num3"))
 if num1>num2 :
     lar=num1
-elif num2>num1 :
+else :
     lar=num2
-     
-if  lar<num3 :
-    lar=num3
 
-print("largest is ",num3)
+if lar<num3 :
+       lar=num3
+       
+print("largest :",lar)
