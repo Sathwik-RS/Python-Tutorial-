6260a6b870042f03f9160a20069553d641650824
Sathwik-RS <rssathwik5@gmail.com>
2026-03-31 21:14:31 +0530

    new code

---
+++ largerof2num.py
@@ -0,0 +1,9 @@
+num1=int(input("enter the value of num1"))
+num2=int(input("enter the value of num2"))
+diff=num1-num2
+if diff<0 :
+        print("num2 is greater than num 1")
+elif diff>0 :
+    print("num1 is greater than num 2")
+else :
+      print("both are equal")
