ebcd97edebdb3661dc4c5913f20dfda82bde4bcb
Sathwik-RS <rssathwik5@gmail.com>
2026-04-01 20:30:57 +0530

    new code

---
+++ validtriangle.py
@@ -0,0 +1,8 @@
+a=int(input("enter the value of side 1"))
+b=int(input("enter the value of side 2"))
+c=int(input("enter the value of side 3"))
+if a+b>c and b+c>a and c+a>b :
+    print("it is a valid traingle")
+    
+
+
