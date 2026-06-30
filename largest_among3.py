7b079c1384d066bf8a85dd99e43fb1effd4e7c6d
Sathwik-RS <rssathwik5@gmail.com>
2026-06-30 10:07:57 +0530

    latest

---
+++ largest_among3.py
@@ -0,0 +1,17 @@
+a=int(input("enter the number 1 "))
+b=int(input("enter the number 2 "))
+c=int(input("enter the number 3")) 
+
+if(a>b):
+    largest=a
+
+else:
+     largest=b
+
+     if(largest<c):
+          largest=c
+          
+          
+
+print(" the largest is ", largest )
+    
